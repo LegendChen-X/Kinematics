f73be7a90924f9b47ddd5c6407d269440410b188
Xiang <chenxiang00825@gmail.com>
2020-07-31 17:57:02 -0400

    create

---
+++ src/catmull_rom_interpolation.cpp
@@ -0,0 +1,12 @@
+#include "catmull_rom_interpolation.h"
+#include <Eigen/Dense>
+
+Eigen::Vector3d catmull_rom_interpolation(
+  const std::vector<std::pair<double, Eigen::Vector3d> > & keyframes,
+  double t)
+{
+  /////////////////////////////////////////////////////////////////////////////
+  // Replace with your code
+  return Eigen::Vector3d(0,0,0);
+  /////////////////////////////////////////////////////////////////////////////
+}
