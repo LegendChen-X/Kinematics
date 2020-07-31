df8a8485b1182a8273c255a49601696d24f78fd4
Xiang <chenxiang00825@gmail.com>
2020-07-31 19:10:53 -0400

    linear_blend_skinning

--- src/linear_blend_skinning.cpp
+++ src/linear_blend_skinning.cpp
@@ -9,6 +9,19 @@ void linear_blend_skinning(
 {
   /////////////////////////////////////////////////////////////////////////////
   // Replace with your code
-  U = V;
+    U.resize(V.rows(),3);
+    for(int i=0;i<V.rows();++i)
+    {
+        Eigen::Vector4d sum(0,0,0,0);
+        Eigen::Vector4d vertex(V(i,0), V(i,1), V(i,2), 1);
+        for(int j=0;j<skeleton.size();++j)
+        {
+            if(skeleton[j].weight_index != -1)
+                sum += T[j] * vertex * W(i, skeleton[j].weight_index);
+        }
+        U(i, 0) = sum(0) / sum(3);
+        U(i, 1) = sum(1) / sum(3);
+        U(i, 2) = sum(2) / sum(3);
+    }
   /////////////////////////////////////////////////////////////////////////////
 }
