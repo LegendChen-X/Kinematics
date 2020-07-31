d8d685110a1d5e9b3709a74712b7e17d271b1a9e
Xiang <chenxiang00825@gmail.com>
2020-07-31 18:29:46 -0400

    forward_kinematics

--- src/forward_kinematics.cpp
+++ src/forward_kinematics.cpp
@@ -2,12 +2,26 @@
 #include "euler_angles_to_transform.h"
 #include <functional> // std::function
 
+Eigen::Affine3d get_transformation(const Skeleton & skeleton, int index)
+{
+    if(skeleton[i].parent_index == -1) return Eigen::Affine3d::Identity();
+    
+    Eigen::Affine3d parent_transformation = get_transformation(skeleton, skeleton[i].parent_index);
+    
+    Eigen::Affine3d rest_T = skeleton[i].rest_T;
+    Eigen::Affine3d rotation = euler_angles_to_transform(skeleton[i].xzx);
+    
+    return parent_transformation * skeleton[i].rest_T * rotation * skeleton[i].rest_T.inverse();
+}
+
+
 void forward_kinematics(
   const Skeleton & skeleton,
   std::vector<Eigen::Affine3d,Eigen::aligned_allocator<Eigen::Affine3d> > & T)
 {
   /////////////////////////////////////////////////////////////////////////////
   // Replace with your code
-  T.resize(skeleton.size(),Eigen::Affine3d::Identity());
+    T.resize(skeleton.size(),Eigen::Affine3d::Identity());
+    for(int i=0;i<skeleton.size();++i) T[i] = get_transformation(skeleton,i);
   /////////////////////////////////////////////////////////////////////////////
 }
