78f9a2db49bb27983ebb65ff90f35a6cdbc18292
Xiang <chenxiang00825@gmail.com>
2020-07-31 18:20:05 -0400

    euler_angles_to_transform

--- src/euler_angles_to_transform.cpp
+++ src/euler_angles_to_transform.cpp
@@ -5,12 +5,32 @@ Eigen::Affine3d euler_angles_to_transform(
 {
   /////////////////////////////////////////////////////////////////////////////
   // Replace with your code
-  Eigen::Affine3d A;
-  A.matrix() << 
+    Eigen::Affine3d A;
+    Eigen::Affine3d B;
+    Eigen::Affine3d C;
+    
+    double th0 = xzx[0]*M_PI/180;
+    double th1 = xzx[1]*M_PI/180;
+    double th2 = xzx[2]*M_PI/180;
+    
+    A.matrix() <<
     1,0,0,0,
-    0,1,0,0,
+    0,cos(th0),-sin(th0),0,
+    0,sin(th0),cos(th0),0,
+    0,0,0,1;
+    
+    B.matrix() <<
+    cos(th1),-sin(th1),0,0,
+    sin(th1),cos(th1),0,0,
     0,0,1,0,
     0,0,0,1;
-  return A;
+    
+    C.matrix() <<
+    1,0,0,0,
+    0,cos(th2),-sin(th2),0,
+    0,sin(th2),cos(th2),0,
+    0,0,0,1;
+    
+    return C*B*A;
   /////////////////////////////////////////////////////////////////////////////
 }
