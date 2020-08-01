d258c3be3724d965d351855a105bc0dbcb0d1ddb
Xiang <chenxiang00825@gmail.com>
2020-07-31 20:33:08 -0400

    end_effectors_objective_and_gradient

--- src/end_effectors_objective_and_gradient.cpp
+++ src/end_effectors_objective_and_gradient.cpp
@@ -14,17 +14,43 @@ void end_effectors_objective_and_gradient(
 {
   /////////////////////////////////////////////////////////////////////////////
   // Replace with your code
-  f = [&](const Eigen::VectorXd & A)->double
-  {
-    return 0.0;
+    f = [&](const Eigen::VectorXd & A)->double
+    {
+      Skeleton copy = copy_skeleton_at(skeleton, A);
+      Eigen::VectorXd tips = transformed_tips(copy, b);
+      return (tips - xb0).dot(tips - xb0);
+    };
+    
+    grad_f = [&](const Eigen::VectorXd & A)->Eigen::VectorXd
+    {
+        Eigen::VectorXd gradient = Eigen::VectorXd::Zero(A.size());
+        Skeleton copy = copy_skeleton_at(skeleton, A);
+        Eigen::VectorXd tips = transformed_tips(copy, b);
+        
+        Eigen::MatrixXd jacobian;
+        kinematics_jacobian(copy,b,jacobian);
+        
+        for(int i=0;i<b.size();++i)
+        {
+            for(int j=0;j<3;++j)
+            {
+                int index = 3*i+j;
+                double dx = 2 * (tip_trans(index)-xb0(index));
+                gradient += jacobian.row(index).transpose() * dx;
+            }
+        }
+        return gradient;
   };
-  grad_f = [&](const Eigen::VectorXd & A)->Eigen::VectorXd
-  {
-    return Eigen::VectorXd::Zero(A.size());
-  };
-  proj_z = [&](Eigen::VectorXd & A)
-  {
-    assert(skeleton.size()*3 == A.size());
-  };
+    
+    proj_z = [&](Eigen::VectorXd & A)
+    {
+        assert(skeleton.size()*3 == A.size());
+        for(int i=0;i<skeleton.size();i++)
+        {
+            A[3*i] = std::max(skeleton[i].xzx_min[0], std::min(skeleton[i].xzx_max[0], A[3*i]));
+            A[3*i+1] = std::max(skeleton[i].xzx_min[1], std::min(skeleton[i].xzx_max[1], A[3*i+1]));
+            A[3*i+2] = std::max(skeleton[i].xzx_min[2], std::min(skeleton[i].xzx_max[2], A[3*i+2]));
+        }
+    };
   /////////////////////////////////////////////////////////////////////////////
 }
