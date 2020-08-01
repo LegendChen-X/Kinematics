e9325dbfc1129d4e5740ca49ddb06dab9cf73e9b
Xiang <chenxiang00825@gmail.com>
2020-07-31 20:20:28 -0400

    copy_skeleton_at

--- src/copy_skeleton_at.cpp
+++ src/copy_skeleton_at.cpp
@@ -5,7 +5,9 @@ Skeleton copy_skeleton_at(
 {
   /////////////////////////////////////////////////////////////////////////////
   // Replace with your code
-  Skeleton copy = skeleton;
-  return copy;
+    Skeleton copy = skeleton;
+    for(int i=0;i<skeleton.size();++i)
+        copy[i].xzx = Eigen::Vector3d(A[3*i],A[3*i+1],A[3*i+2]);
+    return copy;
   /////////////////////////////////////////////////////////////////////////////
 }
